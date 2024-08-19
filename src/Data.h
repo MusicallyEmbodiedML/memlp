20470f26c92d80f27514ea6c5601abf312f31db4
Andrea Martelloni <a.martelloni@sussex.ac.uk>
2024-08-19 15:54:07 +0100

    Made Data.h importable from pure C too

--- src/Data.h
+++ src/Data.h
@@ -1,14 +1,28 @@
 #ifndef __DATA_H__
 #define __DATA_H__
 
+#ifdef __cplusplus
+
+// C++ Includes
 #include <vector>
 
-using num_t = float;
+// C and C++ symbols
+extern "C" {
+#endif
 
+
+typedef float num_t;
+
+#ifdef __cplusplus
+}
+
+// C++-only symbols
 template<typename T>
 using d_vector = std::vector<T>;
 
 template<typename T>
 using nd_vector = std::vector< d_vector<T> >;
+
+#endif
 
 #endif  // __DATA_H__
