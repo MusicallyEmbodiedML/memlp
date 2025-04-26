4aaf6f596ca789d5c95f84633f92bc806d76af30
Andrea Martelloni <a.martelloni@sussex.ac.uk>
2025-04-26 11:30:40 +0100

    Added utils.cpp

---
+++ Utils.cpp
@@ -0,0 +1,12 @@
+#include "Utils.h"
+
+#include <Arduino.h>
+
+extern "C" int getentropy (void * buffer, size_t how_many) {
+    uint8_t* pBuf = (uint8_t*) buffer;
+    while(how_many--) {
+        uint8_t rand_val = rp2040.hwrand32() % UINT8_MAX;
+        *pBuf++ = rand_val;
+    }
+    return 0; // return "no error". Can also do EFAULT, EIO, ENOSYS
+}
